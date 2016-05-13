67b37ef5583dbdf4791de6eac62395d1b3ca5b6d
AndersonBrantes <andersonbrantes@gmail.com>
2016-05-13 00:06:03 -0300

    correção em objeto de exemplo

--- loop_optimization_01.js
+++ loop_optimization_01.js
@@ -1,22 +1,35 @@
-treasureChest
 
-goldCoins: 10,000,
-magicalItem: "Crown of Speed",
-neckLaces: ["ruby", "pearl", "sapphire", "diamond"],
-openLid: function() {
-  alert("Creeeeeak!");
+var treasureChest = {
+  goldCoins: 10.000,
+  magicalItem: "Crown of Speed",
+  neckLaces: ["ruby", "pearl", "sapphire", "diamond"],
+  openLid: function() {
+    alert("Creeeeeak!");
+  }
 }
 
-// 1
 
+// 1
 for(var i = 0; i < treasureChest.neckLaces.length; i++){
-  console.log(treasureChest.neckLaces[i]);
+  console.log("v1 - " + treasureChest.neckLaces[i]);
 }
 
 
 // 2
-
 var x = treasureChest.neckLaces.length;
 for(var i = 0; i < x; i++){
-  console.log(treasureChest.neckLaces[i]);
+  console.log("v2 - " + treasureChest.neckLaces[i]);
+}
+
+
+// 3
+for(var i = 0, x = treasureChest.neckLaces.length; i < x; i++){
+  console.log("v3 - " + treasureChest.neckLaces[i]);
+}
+
+
+// 4
+var list = treasureChest.neckLaces;
+for(var i = 0, x = treasureChest.neckLaces.length; i < x; i++){
+  console.log("v4 - " + list[i]);
 }
