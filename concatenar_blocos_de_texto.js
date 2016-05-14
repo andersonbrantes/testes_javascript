5055bcb9abaf0def66f49cd66ea13728a01c7ac4
AndersonBrantes <andersonbrantes@gmail.com>
2016-05-13 23:32:09 -0300

    otimizando concatenação de blocos grandes de texto

---
+++ concatenar_blocos_de_texto.js
@@ -0,0 +1,20 @@
+var paginaElementos = [ "<!DOCTYPE html>",
+"<html lang='en'>",
+"<head>",
+  "<meta charset='UTF-8'>",
+  "<title>Document</title>",
+"</head>",
+"<body>",
+
+"</body>",
+"</html>" ];
+
+var pagina = "";
+
+// for(var i = 0, x = paginaElementos.length; i < x; i++) {
+//   pagina += paginaElementos[i];
+// }
+
+pagina = paginaElementos.join("\n");
+
+console.log(pagina);
