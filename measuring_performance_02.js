1700931d4e407f92ee544e23b190161e2e327722
AndersonBrantes <andersonbrantes@gmail.com>
2016-05-17 00:30:07 -0300

    completando código da classe de teste

--- measuring_performance_02.js
+++ measuring_performance_02.js
@@ -11,3 +11,21 @@ function SpeedTest(testImplement, testParams, repetitions) {
   this.repetitions = repetitions || 1000;
   this.average = 0;
 }
+
+SpeedTest.prototype = {
+  startTest = function() {
+    var beginTime, endTime, sumTimes = 0;
+
+    for(var i = 0, x = this.repetitions; i < x; i++) {
+      beginTime = +new Date();
+      this.testImplement( this.testParams );
+      endTime = +new Date();
+      sumTimes += endTime - beginTime;
+    }
+
+    this.average = sumTimes / this.repetitions;
+    return console.log("Average execution across " +
+                        this.repetitions + ": " +
+                        this.average);
+  }
+}
