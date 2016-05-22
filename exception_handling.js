5e37b937dff65dcaf9318d4fe995ffa1e4dbe324
AndersonBrantes <andersonbrantes@gmail.com>
2016-05-22 17:15:54 -0300

    Adicionando throw ao tratamento de erros

--- exception_handling.js
+++ exception_handling.js
@@ -1,16 +1,24 @@
 
 try {
   var newHallOfFame = ["Dark Frostring", "Render Tallborn"];
+  if(list === undefined) {
+    throw new ReferenceError();
+  }
+  
+  if( (list instanceof Array) === undefined) {
+    throw new TypeError();
+  }
+
   list.concat(newHallOfFame);
 } catch (error) {
   if (error instanceof ReferenceError) {
-    alert(error + "\n" +
+    console.log(error + "\n" +
           "The Hof roster does not exist as \"list\"!\n" +
           "Check for relocation in master document.");
   }
   if (error instanceof TypeError) {
-    alert(error + "\n" +
-      "Alas, the Hall's list has no concat method \n" +
+    console.log(error + "\n" +
+      "Alas, \"list\" exists, but is no longer as Array.\n" +
       "Check for a disastrous overwrite!");    
   }
 }
